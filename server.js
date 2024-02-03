b262dd16db21e5fcaf0e8d94a675622ccd672c6e
Milan5002 <milan.sana@kalvium.community>
2024-02-03 12:59:41 +0530

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const app = express();
-const port = process.env.PUBLIC_PORT;
+const app = express()
+app.listen(3000)
 
-// define the ping route
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`🚀 server running on PORT: ${port}`);
-  });
-}
+app.get('/ping', (req, res) => 
+  res.send('pong'));
 
-module.exports = app;
+
