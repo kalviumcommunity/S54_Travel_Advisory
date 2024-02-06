8a7e95226fd3f509320cd1c8dc938453b6d6f73e
Milan5002 <milan.sana@kalvium.community>
2024-02-06 16:11:06 +0530

    database connected

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express()
 app.listen(3000)
 
-
+const connectDB=require("./conectionDB")
+connectDB()
 app.get('/ping', (req, res) => 
   res.send('pong'));
 
