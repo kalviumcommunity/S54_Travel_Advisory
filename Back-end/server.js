e8844e0acd998e4093cd6f81402664b036ccf381
Milan5002 <milan.sana@kalvium.community>
2024-02-15 11:16:32 +0530

    Crud Routes Updated

--- Back-end/server.js
+++ Back-end/server.js
@@ -5,12 +5,20 @@ const router = require("./Routes");
 const cors = require("cors");
 app.use(cors());
 app.use("/Travel ", router);
+require("dotenv").config()
+const PORT = process.env.PORT || 5000
 
 
 const connectDB=require("./conectionDB")
 connectDB()
 app.get('/ping', (req, res) => 
   res.send('pong'));
+app.get("/",(req,res)=>{
+  res.send("Hello world, My api is working")
+})
+app.listen(PORT,()=>{
+  console.log(`http://localhost:${PORT}/`)
+})
 
 
 
