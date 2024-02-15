3032995e4cfc465b71bcc3964307c4fbb26d0f9f
Milan5002 <milan.sana@kalvium.community>
2024-02-15 12:59:48 +0530

    data updatted

--- Front-end/reat/src/components/Home.jsx
+++ Front-end/reat/src/components/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://s54-travel-advisory2.onrender.com/Travel")
+      .then((res) => {
+        setData(res.data[0].states);
+        // console.log(res.data[0].states[0].places[0])
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
   return (
     <div>
-      <nav className='nav'>
+      <nav className="nav">
         <a href="/home">Home</a>
         <a href="/login">Login</a>
         <a href="/form">form</a>
@@ -11,8 +24,27 @@ const Home = () => {
       </nav>
       <h1>HOME</h1>
       <h4>This is my landing page for my asap project</h4>
+      {data.map((e, i) => (
+        <>
+          <h1>{e.state}</h1>
+          {e.places.map((places) => (
+            <div style={{
+              border:"2px solid black",
+              marginTop:"40px"
+            }}>
+              <p>{places.name}</p>
+              <p>{places.rating}</p>
+              <p>{places.review}</p>
+              <button><a href={places.google_map_location}>Google map 📌</a></button>
+              
+              <p>{places.infrastructure}</p>
+             <img src={places.img} width={"200px"} alt="" />
+            </div>
+          ))}
+        </>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
