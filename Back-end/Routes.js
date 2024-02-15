e4d11d54a2c743090466ce66865f36ab6c8bd13c
Milan5002 <milan.sana@kalvium.community>
2024-02-15 11:29:57 +0530

    Crud Routes Updated

--- Back-end/Routes.js
+++ Back-end/Routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const Travel = require("./Schema.js");
 
 app.use(express.json());
-
-router.get("/", async (req, res) => {
+router.use(express.json())
+router.get("/Travel", async (req, res) => {
   try {
     const NewPlace = await Travel.find();
     console.log("NewPlace: ", NewPlace);
--- Back-end/server.js
+++ Back-end/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express()
 app.listen(3000)
-const router = require("./Routes");
 const cors = require("cors");
 app.use(cors());
-app.use("/Travel ", router);
+const router = require("./Routes");
+app.use(router)
 require("dotenv").config()
 const PORT = process.env.PORT || 5000
 
