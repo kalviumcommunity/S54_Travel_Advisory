232cde679f5c597d0425361fb5f5bb674c6277c4
Milan5002 <milan.sana@kalvium.community>
2024-02-15 10:35:12 +0530

    done

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express()
 app.listen(3000)
+const router = require("./Routes");
+const cors = require("cors");
+app.use(cors());
+app.use("/Travel ", router);
+
 
 const connectDB=require("./conectionDB")
 connectDB()
 app.get('/ping', (req, res) => 
   res.send('pong'));
+
 
 
 
